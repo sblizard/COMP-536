5bbf340ec3ba992c8d7104d09bbb6bea11b8be2b
Sean Blizard <emailseanblizard@gmail.com>
2025-08-21 16:04:04 -0400

    update typing

--- Problem Set 01/OTP_encrypt_texts.py
+++ Problem Set 01/OTP_encrypt_texts.py
@@ -1,10 +1,10 @@
 import os
 
 #generate a long key using good randomness
-key = os.urandom(10000)
+key: bytes = os.urandom(10000)
 
 #use this to keep track of how much of the one time pad we've used
-usedBytes = 0
+usedBytes: int = 0
 
 with open("texts.txt", 'r') as file:
     for line in file:
@@ -14,11 +14,12 @@ with open("texts.txt", 'r') as file:
             #print a blank line between ciphertexts in the output
             print("")
             #start over because we're going to be encrypting a new text
-            usedBytes=0
+            usedBytes = 0
             continue
         
         #convert the line to bytes
-        bytesLine = bytes(line, 'utf-8')
+        bytesLine: bytes = bytes(line, 'utf-8')
+        print(bytesLine.hex())
 
         #encrypt using OTP
         encryptedLine = bytes(a^b for (a,b) in zip(bytesLine, key[usedBytes:]))
